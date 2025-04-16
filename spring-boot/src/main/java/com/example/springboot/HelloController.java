e9e79fba085aab1ac93f07d7cdba7f63bda938fb
dameun0527 <jisuryu0527@gmail.com>
2025-04-16 20:54:29 +0900

    feat: /echo API 생성
    *4/10 실습: MVC*
    - /echo POST API 구현

--- spring-boot/src/main/java/com/example/springboot/HelloController.java
+++ spring-boot/src/main/java/com/example/springboot/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("/echo")
+    public String echo(@RequestBody String message) {
+        return message;
     }
 }
