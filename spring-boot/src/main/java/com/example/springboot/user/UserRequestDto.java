929239877fffa1c5c1127c3f44393c579a758aca
dameun0527 <jisuryu0527@gmail.com>
2025-04-18 20:33:04 +0900

    feat: dto validation 추가
    *4/12* 실습

--- spring-boot/src/main/java/com/example/springboot/user/UserRequestDto.java
+++ spring-boot/src/main/java/com/example/springboot/user/UserRequestDto.java
@@ -1,11 +1,16 @@
 package com.example.springboot.user;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class UserRequestDto {
+
+    @NotBlank
     private String username;
+
+    @NotBlank
     private String password;
 }
