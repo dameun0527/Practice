8b8415e784939307466cdf5e534c88f3598d69c9
dameun0527 <jisuryu0527@gmail.com>
2025-04-18 17:27:42 +0900

    feat: /greet API 로직 수정
    - Bean 어노테이션 누락 -> 추가함

--- spring-boot/src/main/java/com/example/springboot/greeting/GreetingService.java
+++ spring-boot/src/main/java/com/example/springboot/greeting/GreetingService.java
@@ -1,5 +1,8 @@
 package com.example.springboot.greeting;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GreetingService {
     public String greet() {
         return "Hello Spring";
