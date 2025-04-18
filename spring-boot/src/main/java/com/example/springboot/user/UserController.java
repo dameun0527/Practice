54f4d476b9ca6f6b3f96f619065013eeb8333aa1
dameun0527 <jisuryu0527@gmail.com>
2025-04-18 17:30:14 +0900

    feat: /user POST API 추가
    *4/11 실습*
    - POST API 추가

---
+++ spring-boot/src/main/java/com/example/springboot/user/UserController.java
@@ -0,0 +1,20 @@
+package com.example.springboot.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public ResponseEntity<UserResponseDto> createUser(@RequestBody UserRequestDto userRequestDto) {
+        User user = userService.createUser(userRequestDto);
+        UserResponseDto res = new UserResponseDto(user.getId(), user.getUsername());
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+    }
+}
