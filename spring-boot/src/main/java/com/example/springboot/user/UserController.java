5062341bc5c4efa97786511583f47bd887987716
dameun0527 <jisuryu0527@gmail.com>
2025-04-22 19:25:30 +0900

    feat: dto validation 추가
    *4/12* 실습

--- spring-boot/src/main/java/com/example/springboot/user/UserController.java
+++ spring-boot/src/main/java/com/example/springboot/user/UserController.java
@@ -1,8 +1,10 @@
 package com.example.springboot.user;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,9 +14,13 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/user")
-    public ResponseEntity<UserResponseDto> createUser(@RequestBody UserRequestDto userRequestDto) {
+    public ResponseEntity<UserResponseDto> createUser(
+            @Valid @RequestBody UserRequestDto userRequestDto, BindingResult bindingResult) {
         User user = userService.createUser(userRequestDto);
         UserResponseDto res = new UserResponseDto(user.getId(), user.getUsername());
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
+        }
         return new ResponseEntity<>(res, HttpStatus.CREATED);
     }
 
